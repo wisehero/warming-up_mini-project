8c3e36d0879c4fab16df7d34e4d33c31149ec616
wisehero10 <godjw96@gmail.com>
2024-02-28 20:06:57 +0900

    #1 : 프로젝트 생성

---
+++ src/test/java/com/example/warmingup_miniproject/WarmingUpMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warmingup_miniproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarmingUpMiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
